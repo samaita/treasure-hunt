6e1e3077d838e7c8861c121397d1b41e5dd97d28
samaita <garysamaita@gmail.com>
2021-10-03 21:26:14 +0700

    Revamp all the function with method instead, to avoid update global variable

--- main.go
+++ main.go
@@ -19,38 +19,46 @@ y
 #X#....#
 ######## -- x
 
-# represents an obstacle.
-. represents a clear path.
-X represents the player’s starting position.
+# represents anentity_obstacle.
+. represents a clear entity_path.
+X represents the entity_player’s starting position.
 
-A treasure is hidden within one of the clear path points, and the user must find it.
-User have long range of vision, it able to see left/right/up/down whenever the path is unobstructed.
+A entity_treasure is hidden within one of the clear entity_path points, and the user must find it.
+User have long range of vision, it able to see left/right/up/down whenever the entity_path is unobstructed.
 User restricted to only move in 3 direction, up then right then down.
-The location of the treasure is located randomly every runtime.
-All possible location of the treasure marked as $.
+The location of the entity_treasure is located randomly every runtime.
+All possible location of the entity_treasure marked as $.
 
 */
-
-const (
-	// entity in map
-	path = iota
-	treasure
-	player
-	obstacle
-
-	// direction
+const ( // direction
 	up = iota
 	right
 	down
+	stuck
+)
+
+const (
+	// entity in map
+	entity_path = iota + 1
+	entity_treasure
+	entity_player
+	entity_obstacle
+)
+
+const (
+	// axis
+	axis_x = iota
+	axis_y
+)
+
+const (
+	delay_time = 500 // time used to display a step taken for each movement before continuing
 )
 
 var (
-	treasureMap              = make(map[[2]int]int)
-	treasureMapOriginalState = make(map[[2]int]int)
-	treasureMapSizeXY        = [2]int{8, 6}
-	playerPositionXY         = [2]int{2, 2}
-	playerPosibleMove        = [][2]int{}
-	listCustomObstacle       = [][2]int{
+	mapSize             = [2]int{8, 6}
+	playerStartPosition = [2]int{2, 2}
+	listCustomObstacle  = [][2]int{
 		{3, 2},
 		{3, 4},
 		{4, 4},
@@ -58,269 +66,330 @@ var (
 		{5, 3},
 		{7, 3},
 	}
-	directionOrder = []int{up, right, down}
-	directionTaken = map[int]bool{
-		up:    false,
-		right: false,
-		down:  false,
-	}
 )
 
-func init() {
-	generateMap()
-	spawnPlayer()
-	spawnTreasure()
+type Player struct {
+	Position       [2]int
+	DirectionTaken int
+	FoundTreasure  bool
+	AbleToMove     bool
+}
+
+type Treasure struct {
+	Position [2]int
+}
+
+type TreasureMap struct {
+	Size            [2]int
+	OriginalMapping map[[2]int]int
+	Mapping         map[[2]int]int
 }
 
 func main() {
-	var (
-		treasureFound      bool
-		treasurePositionXY [2]int
-	)
+	player := NewPlayer()
+	treasure := NewTreasure()
+	treasureMap := NewTreasureMap(mapSize)
 
-	// print initial condition
-	renderToTerminal()
-
-	// hide the treasure
-	setPossibleTreasure()
-
-	// print map with hidden treasure
-	renderToTerminal()
-
-	for !treasureFound {
-		treasurePositionXY, treasureFound = checkTreasure()
-		if treasureFound {
-			renderToTerminal()
-			revealMap(treasurePositionXY)
-			renderToTerminal()
-			break
-		} else {
-			renderToTerminal()
-			movePlayer()
-			renderToTerminal()
-		}
-
-	}
-}
-
-// movePlayer update the coordinate of the player limited by predefined direction
-func movePlayer() {
-
-}
-
-func revealMap(treasurePositionXY [2]int) {
-	for coordinate := range treasureMap {
-		if treasureMap[coordinate] == treasure && coordinate != treasurePositionXY {
-			treasureMap[coordinate] = path
-		}
-	}
-}
-
-// checkTreasure check all unobstructed line of X & Y from player position
-func checkTreasure() ([2]int, bool) {
-	var (
-		startX, startY               = playerPositionXY[0], playerPositionXY[1]
-		maxSightX, maxSightY         = treasureMapSizeXY[0], treasureMapSizeXY[1]
-		foundTreasure, foundObstacle = false, false
-	)
-
-	checkPositive, checkNegative := true, true
-	for x := startX + 1; x <= maxSightX; x++ {
-		currentSightXPositive := [2]int{x, startY}
-
-		if checkPositive {
-			foundObstacle, foundTreasure = checkMapEntity(currentSightXPositive)
-			if foundTreasure {
-				return currentSightXPositive, foundTreasure
-			}
-			checkPositive = !foundObstacle
-		}
-
-		if !checkPositive {
-			x = maxSightX
-		}
-	}
-
-	for x := startX - 1; x >= 1; x-- {
-		currentSightXNegative := [2]int{-x, startY}
-
-		if checkNegative {
-			foundObstacle, foundTreasure = checkMapEntity(currentSightXNegative)
-			if foundTreasure {
-				return currentSightXNegative, foundTreasure
-			}
-			checkNegative = !foundObstacle
-		}
-
-		if !checkNegative {
-			x = 1
-		}
-	}
-
-	checkPositive, checkNegative = true, true
-	for y := startY + 1; y <= maxSightY; y++ {
-		currentSightYPositive := [2]int{startX, y}
-		currentSightYNegative := [2]int{startX, y - 2}
-
-		if checkPositive {
-			foundObstacle, foundTreasure = checkMapEntity(currentSightYPositive)
-			if foundTreasure {
-				return currentSightYPositive, foundTreasure
-			}
-			checkPositive = !foundObstacle
-		}
-
-		if checkNegative {
-			foundObstacle, foundTreasure = checkMapEntity(currentSightYNegative)
-			if foundTreasure {
-				return currentSightYNegative, foundTreasure
-			}
-			checkNegative = !foundObstacle
-		}
-
-		if !checkPositive && !checkNegative {
-			y = maxSightY
-		}
-	}
-
-	for y := startY - 1; y >= 1; y-- {
-		currentSightYNegative := [2]int{startX, y}
-
-		if checkNegative {
-			foundObstacle, foundTreasure = checkMapEntity(currentSightYNegative)
-			if foundTreasure {
-				return currentSightYNegative, foundTreasure
-			}
-			checkNegative = !foundObstacle
-		}
-
-		if !checkNegative {
-			y = 1
-		}
-	}
-
-	return [2]int{}, false
-}
-
-func checkMapEntity(position [2]int) (bool, bool) {
-	switch treasureMapOriginalState[position] {
-	case path:
-		treasureMap[position] = path
-	case treasure:
-		return false, true
-	case obstacle:
-		return true, false
-	default:
-		return true, false
-	}
-
-	return false, false
-}
-
-func renderToTerminal() {
-	fmt.Println("\033[2J")
-	fmt.Println(drawTreasureMap())
-
-	time.Sleep(1 * time.Second)
-}
-
-func generateMap() {
-	generateMapObstacleDefault()
-	generateMapObstacleCustom()
-}
-
-// generateMapObstacleDefault putting the obstacle on the outer sandbox
-func generateMapObstacleDefault() {
-	for y := 1; y <= treasureMapSizeXY[1]; y++ {
-		for x := 1; x <= treasureMapSizeXY[0]; x++ {
-			switch true {
-			case x == 1, y == 1, x == treasureMapSizeXY[0], y == treasureMapSizeXY[1]:
-				treasureMap[[2]int{x, y}] = obstacle
-			default:
-				treasureMap[[2]int{x, y}] = path
-			}
-		}
-	}
-}
-
-// generateMapObstacleCustom putting the obstacle by predefined location
-func generateMapObstacleCustom() {
-	for _, customObstacle := range listCustomObstacle {
-		treasureMap[customObstacle] = obstacle
-	}
-}
-
-// spawnTreasure put the treasure in the map randomly. It require several loop to ensure the treasure located on a clear path
-func spawnTreasure() {
-	var (
-		xMin, xMax                           = 1, treasureMapSizeXY[0]
-		yMin, yMax                           = 1, treasureMapSizeXY[1]
-		treasurePositionX, treasurePositionY int
-		treasurePositionXY                   [2]int
-	)
-
+	treasureMap.createMap(listCustomObstacle)
+	treasureMap.setEntity(entity_player, player.Position)
 	for true {
-		rand.Seed(time.Now().UnixNano())
-
-		treasurePositionX = rand.Intn(xMax-xMin) + xMin
-		treasurePositionY = rand.Intn(yMax-yMin) + yMin
-		treasurePositionXY = [2]int{treasurePositionX, treasurePositionY}
-
-		if treasureMap[treasurePositionXY] == path {
-			treasureMap[treasurePositionXY] = treasure
+		treasure.randomizePosition(mapSize[0], mapSize[1])
+		if treasureMap.setEntity(entity_treasure, treasure.Position) {
 			break
 		}
 	}
-}
+	treasureMap.render() // display initial condition with treasure
 
-// setPossibleTreasure hide the actual treasure coordinate to be exposed later
-func setPossibleTreasure() {
-	for coordinate := range treasureMap {
-		treasureMapOriginalState[coordinate] = treasureMap[coordinate]
-		if treasureMap[coordinate] == path {
-			treasureMap[coordinate] = treasure
+	treasureMap.setPossibleTreasure()
+	treasureMap.render() // display map with possible treasure location
+
+	for !player.FoundTreasure {
+		// player see unobstructed path, and determine which is treasure and which is path
+		treasurePositionXY, listPathPosition := player.see(treasureMap)
+		for _, pathPosition := range listPathPosition {
+			treasureMap.setEntity(entity_path, pathPosition)
 		}
+		treasureMap.render()
+
+		if !player.FoundTreasure {
+			// keep moving until found the treasure
+			newPosition, _ := player.move(treasureMap.Mapping)
+			oldPosition := player.Position
+
+			// stop, when player cannot move any longer
+			if newPosition == oldPosition {
+				break
+			}
+
+			// move the player into new position, put path on the older position
+			treasureMap.setEntity(entity_path, oldPosition)
+			treasureMap.setEntity(entity_player, newPosition)
+			treasureMap.render()
+
+			// update player position
+			player.setPosition(newPosition)
+		} else {
+			treasureMap.revealMap(treasurePositionXY)
+			treasureMap.render()
+			break
+		}
+
 	}
 }
 
-// spawnPlayer put the player a.k.a X on the treasure map
-func spawnPlayer() {
-	treasureMap[playerPositionXY] = player
+func NewPlayer() Player {
+	return Player{
+		Position:   playerStartPosition,
+		AbleToMove: true,
+	}
 }
 
-// drawTreasureMap draw the map in a ready print a,.,,
-func drawTreasureMap() string {
+func NewTreasure() Treasure {
+	return Treasure{}
+}
+
+func NewTreasureMap(size [2]int) TreasureMap {
+	return TreasureMap{
+		Size:            size,
+		Mapping:         make(map[[2]int]int),
+		OriginalMapping: make(map[[2]int]int),
+	}
+}
+
+func (p *Player) setPosition(newPosition [2]int) {
+	p.Position = newPosition
+}
+
+// move update the coordinate of the entity_player limited by predefined direction
+func (p *Player) move(treasureMap map[[2]int]int) ([2]int, bool) {
+
+	if p.DirectionTaken == up {
+		newPlayerPositionXY := [2]int{p.Position[0], p.Position[1] + 1}
+		if treasureMap[newPlayerPositionXY] == entity_obstacle {
+			p.DirectionTaken = right
+		} else {
+			return newPlayerPositionXY, true
+		}
+	}
+
+	if p.DirectionTaken == right {
+		newPlayerPositionXY := [2]int{p.Position[0] + 1, p.Position[1]}
+		if treasureMap[newPlayerPositionXY] == entity_obstacle {
+			p.DirectionTaken = down
+		} else {
+			return newPlayerPositionXY, true
+		}
+	}
+
+	if p.DirectionTaken == down {
+		newPlayerPositionXY := [2]int{p.Position[0], p.Position[1] - 1}
+		if treasureMap[newPlayerPositionXY] == entity_obstacle {
+			p.DirectionTaken = stuck
+		} else {
+			return newPlayerPositionXY, true
+		}
+	}
+
+	return p.Position, false
+}
+
+// checkTreasure check all unobstructed line of X & Y from entity_player position
+func (p *Player) see(treasureMap TreasureMap) ([2]int, [][2]int) {
 	var (
-		treasureMapDrawX, treasureMapDrawY string
+		startX, startY                  = p.Position[0], p.Position[1]
+		treasurePosition, treasureFound [2]int
+		listPathPosition, pathFound     [][2]int
 	)
 
-	for y := 1; y <= treasureMapSizeXY[1]; y++ {
-		treasureMapDrawX = ""
-		if y < treasureMapSizeXY[1] {
-			treasureMapDrawX = "\n"
-		}
-		for x := 1; x <= treasureMapSizeXY[0]; x++ {
-			treasureMapDrawX = treasureMapDrawX + convertIntToEntity(treasureMap[[2]int{x, y}])
-		}
-		treasureMapDrawY = treasureMapDrawX + treasureMapDrawY
+	// see all entity in x axis with same y axis
+	treasurePosition, pathFound = checkMap(treasureMap, startX+1, startY, 1, axis_x)
+	if treasureMap.OriginalMapping[treasurePosition] == entity_treasure {
+		treasureFound = treasurePosition
+	}
+	listPathPosition = append(listPathPosition, pathFound...)
 
+	// see all entity in -x axis with same y axis
+	treasurePosition, pathFound = checkMap(treasureMap, startX-1, startY, -1, axis_x)
+	if treasureMap.OriginalMapping[treasurePosition] == entity_treasure {
+		treasureFound = treasurePosition
+	}
+	listPathPosition = append(listPathPosition, pathFound...)
+
+	// see all entity in y axis with same x axis
+	treasurePosition, pathFound = checkMap(treasureMap, startY+1, startX, 1, axis_y)
+	if treasureMap.OriginalMapping[treasurePosition] == entity_treasure {
+		treasureFound = treasurePosition
+	}
+	listPathPosition = append(listPathPosition, pathFound...)
+
+	// see all entity in -y axis with same x axis
+	treasurePosition, pathFound = checkMap(treasureMap, startY-1, startX, -1, axis_y)
+	if treasureMap.OriginalMapping[treasurePosition] == entity_treasure {
+		treasureFound = treasurePosition
+	}
+	listPathPosition = append(listPathPosition, pathFound...)
+
+	if treasureMap.OriginalMapping[treasureFound] == entity_treasure {
+		p.FoundTreasure = true
 	}
 
-	return treasureMapDrawY
+	return treasureFound, listPathPosition
+}
+
+func checkMap(treasureMap TreasureMap, startAxis int, staticAxis int, addValue int, typeAxis int) ([2]int, [][2]int) {
+	var (
+		check            = true
+		treasurePosition [2]int
+		pathPosition     [][2]int
+		currentPosition  [2]int
+	)
+	for check {
+		if typeAxis == axis_x {
+			currentPosition = [2]int{startAxis, staticAxis}
+		} else {
+			currentPosition = [2]int{staticAxis, startAxis}
+		}
+
+		if check {
+			switch treasureMap.OriginalMapping[currentPosition] {
+			case entity_path:
+				pathPosition = append(pathPosition, currentPosition)
+			case entity_treasure:
+				treasurePosition = currentPosition
+			case entity_obstacle:
+				check = false
+			default:
+				check = false
+			}
+		}
+		startAxis += addValue
+	}
+
+	return treasurePosition, pathPosition
 }
 
 // convertIntToEntity convert code constant of entity to a map drawn entity
 func convertIntToEntity(code int) string {
 	switch code {
-	case path:
+	case entity_path:
 		return "."
-	case obstacle:
+	case entity_obstacle:
 		return "#"
-	case player:
+	case entity_player:
 		return "X"
-	case treasure:
+	case entity_treasure:
 		return "$"
 	default:
 		return "."
+	}
+}
+
+func (tm *TreasureMap) render() {
+	var (
+		treasureMapDrawPerLine, treasureMapDrawComplete string
+	)
+
+	for y := 1; y <= tm.Size[1]; y++ {
+		treasureMapDrawPerLine = ""
+		if y < tm.Size[1] {
+			treasureMapDrawPerLine = "\n"
+		}
+		for x := 1; x <= tm.Size[0]; x++ {
+			treasureMapDrawPerLine = treasureMapDrawPerLine + convertIntToEntity(tm.Mapping[[2]int{x, y}])
+		}
+		treasureMapDrawComplete = treasureMapDrawPerLine + treasureMapDrawComplete
+
+	}
+
+	// fmt.Println("\033[2J")
+	fmt.Println(treasureMapDrawComplete)
+	time.Sleep(delay_time * time.Millisecond)
+}
+
+// generateMapObstacleDefault putting theentity_obstacle on the outer sandbox
+func (tm *TreasureMap) generate() {
+	for y := 1; y <= tm.Size[1]; y++ {
+		for x := 1; x <= tm.Size[0]; x++ {
+			switch true {
+			case x == 1, y == 1, x == tm.Size[0], y == tm.Size[1]:
+				tm.Mapping[[2]int{x, y}] = entity_obstacle
+			default:
+				tm.Mapping[[2]int{x, y}] = entity_path
+			}
+		}
+	}
+}
+
+// generateMapObstacleCustom putting theentity_obstacle by predefined location
+func (tm *TreasureMap) addObstacle(listCustomObstacle [][2]int) {
+	for _, customObstacle := range listCustomObstacle {
+		tm.Mapping[customObstacle] = entity_obstacle
+	}
+}
+
+// spawnTreasure put the entity_treasure in the map randomly. It require several loop to ensure the entity_treasure located on a clear entity_path
+func (t *Treasure) randomizePosition(sizeX, sizeY int) {
+	var (
+		xMin, xMax                           = 1, sizeX
+		yMin, yMax                           = 1, sizeY
+		treasurePositionX, treasurePositionY int
+		treasurePositionXY                   [2]int
+	)
+
+	rand.Seed(time.Now().UnixNano())
+
+	treasurePositionX = rand.Intn(xMax-xMin) + xMin
+	treasurePositionY = rand.Intn(yMax-yMin) + yMin
+	treasurePositionXY = [2]int{treasurePositionX, treasurePositionY}
+
+	t.Position = treasurePositionXY
+}
+
+func (tm *TreasureMap) createMap(obstacle [][2]int) {
+	tm.generate()
+	tm.addObstacle(obstacle)
+}
+
+// spawnPlayer put the entity_player a.k.a X on the entity_treasure map
+func (tm *TreasureMap) setEntity(entity int, position [2]int) bool {
+	switch tm.Mapping[position] {
+	case entity_obstacle:
+		return false
+	case entity_path:
+		if entity == entity_treasure || entity == entity_path || entity == entity_player {
+			tm.Mapping[position] = entity
+			return true
+		}
+	case entity_treasure:
+		if tm.OriginalMapping[position] == entity_treasure {
+			return false
+		} else {
+			tm.Mapping[position] = entity
+			return true
+		}
+	case entity_player:
+		if entity == entity_path {
+			tm.Mapping[position] = entity
+			return true
+		}
+	}
+	return false
+}
+
+// setPossibleTreasure hide the actual entity_treasure coordinate to be exposed later
+func (tm *TreasureMap) setPossibleTreasure() {
+	for coordinate := range tm.Mapping {
+		tm.OriginalMapping[coordinate] = tm.Mapping[coordinate]
+		if tm.Mapping[coordinate] == entity_path {
+			tm.Mapping[coordinate] = entity_treasure
+		}
+	}
+}
+
+func (tm *TreasureMap) revealMap(treasurePositionXY [2]int) {
+	for coordinate := range tm.Mapping {
+		if tm.Mapping[coordinate] == entity_treasure && coordinate != treasurePositionXY {
+			tm.Mapping[coordinate] = entity_path
+		}
 	}
 }
